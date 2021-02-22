b8822109d8f7edd0fb3897f084c158280bf8b783
Ben Thompson <btomo86@gmail.com>
2021-02-22 17:05:48 +0000

    added state to filter robots

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
+import App from "./App";
 import "tachyons";
-import CardList from "./CardList";
-import { robots } from "./Robots";
 
 ReactDOM.render(
   <React.StrictMode>
-    <CardList robots={robots} />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
